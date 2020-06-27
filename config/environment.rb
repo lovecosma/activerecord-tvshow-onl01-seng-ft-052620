727b73dcd94e6cb8f27f5888aa508fca0b874f59
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-27 23:26:11 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -10,7 +10,7 @@ Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| requi
 
 DB = ActiveRecord::Base.establish_connection({
   adapter: 'sqlite3',
-  database: 'db/tvshows.sqlite'
+  database: 'db/shows.sqlite'
 })
 
 if ENV["ACTIVE_RECORD_ENV"] == "test"
