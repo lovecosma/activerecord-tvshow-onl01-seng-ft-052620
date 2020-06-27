fae9cce05c9c0193d2115961a23086cec6fbc1b4
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-27 23:43:37 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -9,7 +9,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.lowest_rating
-    slef.where(:rating == self.minimum(:rating)).take
+    self.where(:rating == self.minimum(:rating)).take
   end
 
 
