80d9fc300671b9f21a30f63c843ac450c8574ea2
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-27 23:04:49 +0000

    Automatically backed up by Learn

--- db/migrate/20200627225903_create_shows.rb
+++ db/migrate/20200627225903_create_shows.rb
@@ -2,7 +2,7 @@ class CreateShows < ActiveRecord::Migration[5.2]
   def change
     create_table tvshows do |t|
       t.string :name
-      t.string
+      t.string :day
       t.string
       t.integer
     end
