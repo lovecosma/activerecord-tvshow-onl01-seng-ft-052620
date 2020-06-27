e0477413c5187049c93b169554f83ba342511905
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-27 23:20:35 +0000

    Automatically backed up by Learn

---
+++ db/migrate/002_add_season_to_shows.rb
@@ -0,0 +1,10 @@
+class CreateShows < ActiveRecord::Migration[5.2]
+  def change
+    add_column :tvshows do |t|
+      t.string :name
+      t.string :day
+      t.string :network
+      t.integer :rating
+    end
+  end
+end
