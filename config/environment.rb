c641972847ede0e335f25d358ae9c9f1f41e3a04
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-28 00:25:13 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,8 @@
 require "bundler/setup"
 require 'yaml'
 require 'active_record'
+require_relative '../app/models/show.rb'
+require_relative '../app/show_controller.rb'
 
 Bundler.require
 
@@ -9,9 +11,21 @@ Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| requi
 
 DB = ActiveRecord::Base.establish_connection({
   adapter: 'sqlite3',
-  database: 'db/tvshows.db'
+  database: 'db/shows.sqlite'
 })
 
 if ENV["ACTIVE_RECORD_ENV"] == "test"
   ActiveRecord::Migration.verbose = false
 end
+
+sql = <<-SQL
+CREATE TABLE IF NOT EXISTS shows (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  day TEXT,
+  network TEXT,
+  rating INTEGER
+  )
+SQL
+
+ActiveRecord::Base.connection.execute(sql)
